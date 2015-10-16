a381453454edf8830b9514c461ab159b7f806dc0
Carlton Hanna <nullpointer0x00@gmail.com>
2015-10-15 22:50:21 -0500

    Add files

---
+++ README.md
@@ -0,0 +1,1 @@
+Chrome plugin for strava user to peek at followers.
---
+++ options_onload.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    load_options();
+
+    $('#save').click(function(){
+	save_strava_access_token();
+	load_options();
+    });
+});
